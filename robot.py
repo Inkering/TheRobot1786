9ca57c95efdb94bc7666d694ce10b0331312e248
inkering <inkering@gmail.com>
2016-07-20 20:13:41 -0400

    Added some simulation configs and syntax fixes

--- robot.py
+++ robot.py
@@ -1,104 +1,106 @@
 #!/usr/bin/env python3
+"""
+	This is a good foundation to build your robot code on
+"""
 
 import wpilib
 import math
 
-TRIGGER =    1  #Trigger button number
-THUMB =      2  #Thumb button number
-RAMP_RAISE = 5  #Button for raising ramp
-RAMP_LOWER = 3  #Buttom to lower ramp
-UNJAM =      11 #Button to run unjam command
+TRIGGER = 1
+THUMB = 2
+RAMP_RAISE = 5
+RAMP_LOWER = 3
+UNJAM = 11
+DEADZONE_RADIUS = 0.05
+TURN_FACTOR = 1.5
 
-DEADZONE_RADIUS = 0.05 # Deadzone Radius prevents tiny twitches in the joystick's value from
-					   # affecting the robot.  Use this for cleaning up drive train and shooter.
-					   # Also used for detecting changes in an axis' value.
+class MyRobot(wpilib.IterativeRobot):
 
-TURN_FACTOR = 1.5      # Left(x,y)  = y*(1 + TF*x)  :  x < 0
-					   #	= y 			  :  x >= 0
-					   # Right(x,y) = y			  :  x < 0
-					   #    = y*(1 - TF*x)  :  x >= 0
-
-class Robot(wpilib.IterativeRobot):
 	def robotInit(self):
 		"""
-        This function is called upon program startup and
-        should be used for any initialization code.
-        """
-        #shooter1.Enable()
-        #shooter2.Enable()
-        left_drive.SetInverted(true)
-        right_drive.SetInverted(true)
+		This function is called upon program startup and
+		should be used for any initialization code.
+		"""
+		self.left_drive = wpilib.TalonSRX(0)
+		self.right_drive = wpilib.TalonSRX(1)
+		self.shooter1 = wpilib.CANTalon(11)
+		self.shooter2 = wpilib.CANTalon(10)
+		self.ramp = wpilib.CANTalon(12)
+		self.arms = wpilib.CANTalon(13)
+		self.drive = wpilib.RobotDrive(self.left_drive,self.right_drive)
+		#Shooter object
+		self.driver_stick = wpilib.Joystick(0)
+		self.operator_stick = wpilib.Joystick(1)
+		self.left_drive.setInverted(True)
+		self.right_drive.setInverted(True)
 
-        inverting = false
-        pickupRunning = false
-        ramping = false
-        shooting = false
-        unjamming = false
-        arming = false
-        shooter_power = 0
-        arcade = false
+		global inverting
+		inverting = False
+		pickupRunning = False
+		ramping = False
+		shooting = False
+		unjamming = False
+		arming = False
+		shooterPower = 0
+		global arcade
+		arcade = False
+	def autonomousInit(self):
+		"""This function is run once each time the robot enters autonomous mode."""
+		self.auto_loop_counter = 0
 
-    #Robotpy docs said these instances need to be after robotInit()
-    left_drive = wpilib.TalonSRX(0)
-	right_drive = wpilib.TalonSRX(1)
-	shooter1 = wpilib.CANTalon(11)
-	shooter2 = wpilib.CANTalon(10)
-	ramp = wpilib.CANTalon(12)
-	arms = wpilib.CANTalon(13)
-	drive = wpilib.RobotDrive(left_drive, right_drive)
-	# shooter = Shooter(shooter1, shooter2, ramp) #Must add shooter class
-	driver_stick = wpilib.Joystick(0)
-	operator_stick = wpilib.Joystick(1)
-    def autonomousInit(self):
-    	"""This function is run once each time the robot enters autonomous mode."""
-    def autonomousPeriodic(self):
-    	"""This function is called periodically during autonomous."""
-    def teleopPeriodic(self):
-    	"""This function is called periodically during operator control."""
-    	drive.ArcadeDrive(-driver_stick.GetY(), -driver_stick.GetX()*0.75)
-    	if(driver_stick.GetRawButton(7))
-    		arcade = true
-    	if(driver_stick.GetRawButton(8))
-    		arcade = false
-    	if (arcade)
-    		drive.ArcadeDrive(driver_stick)
-    	else
-    		if(driver_stick.GetRawButton(THUMB))
-    			left = driver_stick.GetTwist()
-    			right = -driver_stick.GetTwist()
-    			drive.TankDrive(left, right)
-    		else
-    			wpilib.UpdateDrive()
-    	#ARMS & Shooter I ignored for now
-	def SaneThrottle(rawThrottle):
+	def autonomousPeriodic(self):
+		"""This function is called periodically during autonomous."""
+
+		# Check if we've completed 100 loops (approximately 2 seconds)
+		if self.auto_loop_counter < 100:
+			self.drive.drive(-0.5, 0) # Drive forwards at half speed
+			self.auto_loop_counter += 1
+		else:
+			self.drive.drive(0, 0)    #Stop robot
+
+	def saneThrottle(rawThrottle):
 		return ((1.0 - rawThrottle) / 2.0)
-    def testPeriodic(self):
-    	"""This function is called periodically during test mode."""
-    	#live window
-    def SimpleDrive()
-    	x = -driver_stick.getX()
-    	y = -driver_stick.getY()
-    	left = 0
-    	right = 0
 
-    	if (x > 0)
-    		right = y
-    		left = (1 - x * TURN_FACTOR) * y
-    	else
-    		left = y
-    		right = (1 + x * TURN_FACTOR) * y
+	def updateDrive():
+		x = -self.driver_stick.getX()
+		y = -self.driver_stick.getY()
+		if (x > 0):
+			left = y * saneThrottle(driver_stick.getThrottle())
+			right = (1 + x) * y * saneThrottle(driver_stick.getThrottle())
+			drive.tankDrive(left, right)
 
-    	drive.TankDrive(left, right)
-    def UpdateDrive
-    	x = -driver_stick.GetX()
-    	y = -driver_stick.GetY()
-    	if (x > 0)
-    		right = y * SaneThrottle(driver_stick.GetThrottle())
-    		left = (1 - x) * y * SaneThrottle(driver_stick.GetThrottle())
-    		drive.TankDrive(left, right)
-    	else
-    		left = y * SaneThrottle(driver_stick.GetThrottle())
-    		right = (1 + x) * y * SaneThrottle(driver_stick.GetThrottle())
-    		drive.TankDrive(left, right)
+	def teleopPeriodic(self):
+		"""This function is called periodically during operator control."""
+		self.drive.arcadeDrive(self.driver_stick)
+		if(self.driver_stick.getRawButton(7)):
+			global arcade
+			arcade = True
+		if(self.driver_stick.getRawButton(8)):
+			global arcade
+			arcade = False
+		if(arcade):
+			self.drive.arcadeDrive(self.driver_stick)
+		else:
+			if(self.driver_stick.getRawButton(THUMB)):
+				left = self.driver_stick.getTwist()
+				right = -self.driver_stick.getTwist()
+				self.drive.tankDrive(left, right)
+			else:
+				self.updateDrive
+		if(self.driver_stick.getRawButton(TRIGGER)) and not inverting:
+			self.left_drive.setInverted(not self.left_drive.getInverted)
+			self.right_drive.setInverted(not self.right_drive.getInverted)
+			global inverting
+			inverting = True
+		elif not self.driver_stick.getRawButton(TRIGGER):
+			global inverting
+			inverting = False
+
+	def testPeriodic(self):
+		"""This function is called periodically during test mode."""
+		wpilib.LiveWindow.run()
+
+
+
 if __name__ == "__main__":
-    wpilib.run(Robot)
+	wpilib.run(MyRobot)
