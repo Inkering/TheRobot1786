8c1c16efe7ebe0286c6d70ef4ae9b2f3badaac4d
inkering <inkering@gmail.com>
2016-07-21 21:15:57 -0400

    Added shooter & ramp functionality, gitignored

--- robot.py
+++ robot.py
@@ -9,11 +9,6 @@ THUMB = 2
 RAMP_RAISE = 5
 RAMP_LOWER = 3
 UNJAM = 11
-DEADZONE_RADIUS = 0.05
-TURN_FACTOR = 1.5
-
-
-
 
 class MyRobot(wpilib.IterativeRobot):
     def robotInit(self):
@@ -21,16 +16,31 @@ class MyRobot(wpilib.IterativeRobot):
 		This function is called upon program startup and
 		should be used for any initialization code.
 		"""
-
         self.left_drive = wpilib.TalonSRX(0)
         self.right_drive = wpilib.TalonSRX(1)
         self.drive = wpilib.RobotDrive(self.left_drive, self.right_drive)
+
+        self.shooter1 = wpilib.CANTalon(11)
+        self.shooter2 = wpilib.CANTalon(10)
+        self.ramp = wpilib.CANTalon(12)
+        self.shooter = shooter.shooter(self.shooter1, self.shooter2, self.ramp)
+
         self.driver_stick = wpilib.Joystick(0)
         self.operator_stick = wpilib.Joystick(1)
+
+        self.shooter1.enable()
+        self.shooter2.enable()
+
         self.left_drive.setInverted(True)
         self.right_drive.setInverted(True)
 
         self.inverting = False
+        self.pickupRunning = False
+        self.ramping = False
+        self.shooting = False
+        self.unjamming = False
+        self.arming = False
+        self.shooterPower = 0
         self.arcade = False
 
     def autonomousInit(self):
@@ -49,11 +59,12 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        self.drive.arcadeDrive(self.driver_stick)
-        # if(self.driver_stick.getRawButton(7)):
-        #	arcade = True
-        # if(self.driver_stick.getRawButton(8)):
-        #	arcade = False
+        #self.drive.arcadeDrive(self.driver_stick)
+        self.drive.arcadeDrive(-self.driver_stick.getY(), -self.driver_stick.getX() * 0.75)
+        if(self.driver_stick.getRawButton(7)):
+            self.arcade = True
+        if(self.driver_stick.getRawButton(8)):
+            self.arcade = False
         if (self.arcade):
             self.drive.arcadeDrive(self.driver_stick)
         else:
@@ -63,12 +74,44 @@ class MyRobot(wpilib.IterativeRobot):
                 self.drive.tankDrive(left, right)
             else:
                 self.updateDrive()
-        if (self.driver_stick.getRawButton(TRIGGER)) and (not self.inverting):
-            self.left_drive.setInverted(not self.left_drive.getInverted)
-            self.right_drive.setInverted(not self.right_drive.getInverted)
+        if (not self.ramping and self.operator_stick.getRawButton(RAMP_RAISE)):
+            self.shooter.raiseRamp()
+            self.ramping = True
+        elif (not self.ramping and self.operator_stick.getRawButton(RAMP_LOWER)):
+            self.shooter.lowerRamp()
+            self.rampign = True
+        elif (self.ramping and not self.operator_stick.getRawButton(RAMP_LOWER) and not self.operator_stick.getRawButton(RAMP_RAISE)):
+            self.shooter.stopRamp()
+            self.ramping = False
+        if (not self.unjamming and self.operator_stick.getRawButton(UNJAM)):
+            self.unjamming = True
+            self.shooter.unJam()
+        elif (not self.unjamming and self.operator_stick.getRawButton(TRIGGER)):
+            self.shooter.shootLow()
+            self.unjamming = True
+        elif (self.unjamming and not self.operator_stick.getRawButton(UNJAM) and not self.operator_stick.getRawButton(TRIGGER)):
+            self.shooter.pickUp(False)
+            self.unjamming = False
+        #comment here
+        if(self.operator_stick.getRawButton(THUMB) and not self.pickupRunning):
+            #added True Bool to self.shooter.pickUp(True)
+            self.shooter.pickUp(True)
+            self.pickupRunning = True
+        elif (not self.operator_stick.getRawButton(THUMB) and self.pickupRunning):
+            self.shooter.pickUp(False)
+            self.pickupRunning = False
+
+        if (self.driver_stick.getRawButton(TRIGGER) and not self.inverting):
+            self.left_drive.setInverted(not self.left_drive.getInverted())
+            self.right_drive.setInverted(not self.right_drive.getInverted())
             self.inverting = True
         elif not (self.driver_stick.getRawButton(TRIGGER)):
             self.inverting = False
+
+        self.opThrottle = self.saneThrottle(self.operator_stick.getThrottle())
+
+        if (not self.pickupRunning and not self.unjamming):
+            self.shooter.setPower(self.opThrottle)
 
     def testPeriodic(self):
         """This function is called periodically during test mode."""
---
+++ shooter.py
@@ -0,0 +1,78 @@
+from enum import Enum
+import wpilib
+
+PICKUP_POWER = 0.75
+LAUNCH_POWER = 1
+SPINUP_TIME = 1.5 #Seconds
+LAUNCH_TIME = 0.5
+
+class ShooterState(Enum):
+    READY = 1,
+    ON_FIRE = 2,
+    SPINNING_UP = 3,
+    LAUNCH = 4,
+    LAUNCHING = 5,
+    RESETTING = 6
+
+class shooter():
+    def __init__(self, talon1, talon2, ramp):
+        self.launcher = talon1
+        self.pickup = talon2
+        self.ramp = ramp
+        self.ready = True
+        self.state = ShooterState.READY
+        self.shotClock = wpilib.Timer
+    def deleteShooter(self):
+        del self.launcher
+        del self.pickup
+        del self.ramp
+    def stopShooter(self):
+        self.ready = True
+        self.ramp.set(0)
+        self.pickup.set(0)
+    def lowerRamp(self):
+        self.ramp.set(-0.5)
+        #if (self.ramp.isFwdLimitSwitchClosed()):
+        #
+        #else:
+        #
+    def stopRamp(self):
+        self.ramp.set(0)
+    def shoot(self):
+        if self.state == ShooterState.READY:
+            self.state = ShooterState.SPINNING_UP
+            self.ramp.set(-1)
+            self.launcher.set(PICKUP_POWER)
+            self.shotClock.reset()
+            self.shotClock.start()
+            #break alternative?
+        elif self.state == ShooterState.SPINNING_UP:
+            if (self.shotClock.get() > SPINUP_TIME):
+                self.state = ShooterState.LAUNCH
+                self.shotClock.reset()
+                self.shotClock.start()
+            else:
+                message = "Goku noises"
+        elif self.state == ShooterState.LAUNCH:
+            self.ramp.set(1)
+            self.state = ShooterState.LAUNCHING
+        elif self.state == ShooterState.LAUNCHING:
+            if (self.shotClock.get() > LAUNCH_TIME):
+                self.state = ShooterState.RESETTING
+        elif self.state == ShooterState.RESETTING:
+            self.ramp.set(0)
+            self.launcher.set(0)
+            self.pickup.set(0)
+            self.state = ShooterState.READY
+        elif self.state == ShooterState.ON_FIRE:
+            message = "Something is wrong here"
+    def pickUp(self, pickUpState = True):
+        self.pickup.set(pickUpState * PICKUP_POWER)
+        self.launcher.set(pickUpState * PICKUP_POWER * -0.75)
+    def unJam(self):
+        self.pickup.set(PICKUP_POWER * -0.75)
+    def shootLow(self):
+        self.pickup.set(-1)
+    def setPower(self, power):
+        self.pickup.set(power)
+        self.launcher.set(power)
